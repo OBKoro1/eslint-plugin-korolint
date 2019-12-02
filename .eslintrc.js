657d7af2fbf6eb2d4f97fa4f88679938d04c9255
OBKoro1 <1677593011@qq.com>
2019-12-02 10:57:01 +0800

    update 更改仓库名 在导出规则时不能多个名字

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: OBKoro1
+ * @Description: 
+ * @Date: 2019-11-28 19:55:08
+ * @LastEditors: OBKoro1
+ * @LastEditTime: 2019-11-30 14:13:03
+ * @FilePath: /eslint-plugin-koro-create/.eslintrc.js
+ */
+module.exports = {
+  plugins: ['koro-create'], // 增加一项该配置
+};
